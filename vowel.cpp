14afdd79a804b5891d3537c9f5f0fd1b02b66756
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-14 15:20:21 +0530

    Update and rename odd or even.cpp to vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    char n;
+    cout<<"enter the value";
+    cin>>n;
+       if(n=='a'||n=='A'||n=='e'||n=='E'||n=='i'||n=='I'||n=='o'||n=='O'||n=='u'||n=='U')
+    {
+    cout<<"it is vowel";
+    }
+    else 
+    {
+    cout<<"it is not vowel";
+    }
+    
+return 0;
+}
