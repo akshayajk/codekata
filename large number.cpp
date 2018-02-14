c129b072371702f2569dda1cb78ce77da676ff81
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-14 16:23:10 +0530

    Update and rename alphabet.cpp to large number.cpp

---
+++ large number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a=100,b=150,c=10;
+   
+       if(a>b&&a>c)
+    {
+    cout<<"a is large";
+    }
+    else if(b>c)
+    {
+    cout<<"b is large";
+    }
+    else
+    {
+    	cout<<"c is large";
+    }
+return 0;
+}
