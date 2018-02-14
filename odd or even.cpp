fdbcacde1915463851b6b05f43bd42ca0ef10280
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-14 15:12:33 +0530

    Create odd or even.cpp

---
+++ odd or even.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"enter the value";
+    cin>>n;
+       if(n%2==0&&(n<=1&&n>100000))
+    {
+    cout<<"it is even";
+    }
+    else 
+    {
+    cout<<"it is odd";
+    }
+    
+return 0;
+}
