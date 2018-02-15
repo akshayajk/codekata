9ba3bf810ff59bdba8130368a1fbe17db6e2317b
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 12:28:00 +0530

    Create count5.cpp

---
+++ count5.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n,a[5];
+    cout<<"enter the value";
+    cin>>n;
+    for(i=1;i<=5;i++)
+    {
+        a[i]=i*n;
+        cout<<a[i]<<endl;
+        }
+}
