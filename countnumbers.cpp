57143326f5203997eedff1339ddd274b7fb8e534
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 10:24:31 +0530

    Create countnumbers.cpp

---
+++ countnumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+int n,a=0;
+cout<<"Enter any num : ";
+cin>>n;
+while(n>0)
+{
+n=n/10;
+a++;
+}
+cout<<"\n no. of digits in given number is: "<<a;
+return 0;
+}
