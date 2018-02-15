55c98b4a9f84423763ed2c69f45b5b1a94643d37
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 11:46:17 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+class Prime
+{
+    int i,n,count=0;
+    public:
+    void calculate()
+    {
+        cout<<"enter the number";
+        cin>>n;
+        for(i=2;i<n;i++)
+        {
+            if(n%i==0)
+            {
+                count++;
+                break;
+            }
+        
+        }
+    }
+    public:
+    void display()
+    {
+        if(count==0)
+        {
+            cout<<"it is prime";
+        }
+        else
+        {
+            cout<<"it is not prime";
+        }
+    
+        
+    }
+};
+int main()
+{
+    Prime p;
+    p.calculate();
+    p.display();
+
+    return 0;
+}
+
