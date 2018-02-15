4ca1877dae458dab7035f18d124d351b6d2d9f01
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 09:36:24 +0530

    Create natural.cpp

---
+++ natural.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class Program
+{
+	int i,n,sum=0;
+	public:
+	void display()
+	{
+		cout<<"enter your number"<<endl;
+		cin>>n;
+		for(i=1;i<=n;i++)
+		{
+			sum=sum+i;
+			cout<<"the value is"<<sum;
+		
+		}
+	}
+};
+int main() {
+Program a;
+a.display();
+return 0;
+}
