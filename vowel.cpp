487e131de618475d011356123501934ad92e348b
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 09:55:07 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  char a;
+  cout<<"Enter the input";
+  cin>>a;
+  if( a=='a'|| a=='e'|| a=='i'|| a== 'o'|| a=='u'||
+  a == 'A'||a== 'E'||a =='I'||a == 'O'||a =='U')
+  {
+      cout<<"it is vowel";
+  }
+  else
+  {
+      cout<<"it is not vowel";
+  }
+}
