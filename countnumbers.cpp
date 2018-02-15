4ed9c468f1f6222a4502cb30ecb84e55349fe882
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-15 10:40:23 +0530

    Update countnumbers.cpp

--- countnumbers.cpp
+++ countnumbers.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-int n,a=0;
+int m,a=0;
 cout<<"Enter any num : ";
-cin>>n;
-while(n>0)
+cin>>m;
+while(m>0)
 {
-n=n/10;
+m=m/10;
 a++;
 }
 cout<<"\n no. of digits in given number is: "<<a;
