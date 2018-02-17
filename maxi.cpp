ba8a1d906e86eddb8219ed42bb5cc7695d6b90d5
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-17 16:02:09 +0530

    Update maxi.cpp

--- maxi.cpp
+++ maxi.cpp
@@ -1,29 +1,24 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-    int i, n;
-    float arr[100];
-
-    cout << "Enter total number of elements: ";
-    cin >> n;
-    cout << endl;
-
-    for(i = 0; i < n; ++i)
+    int i,n,arr[50],large;
+    cout<<"enter the numbeer of elements";
+    cin>>n;
+    cout<<"enter the elements";
+    for(i=0;i<n;++i)
     {
-       cout << "Enter Number " << i + 1 << " : ";
-       cin >> arr[i];
+        
+        cin>>arr[i];
     }
 
-    for(i = 1;i < n; ++i)
+    for(i=1;i<n;++i)
     {
-       if(arr[0] < arr[i])
-           arr[0] = arr[i];
+        if(arr[i]>arr[0])
+                    arr[0]=arr[i];
+        
+   
     }
-    cout << "Largest element = " << arr[0];
-
+     cout<<"the large value is"<<arr[0];
     return 0;
 }
-
-
