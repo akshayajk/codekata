6b3e2bde75fa7b426edd731c40e82631e22c02ab
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-19 20:37:16 +0530

    Create multiples68.cpp

---
+++ multiples68.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int a,n,i;
+  cout<<"Enter your number";
+  cin>>a;
+ for(i=0;i<=n;i++)
+{
+    if(a%7==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+}
+}
