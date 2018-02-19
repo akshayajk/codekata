8c557e9d6b521e623d302950c4b0ca8932014911
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-19 20:29:40 +0530

    Create odd64.cpp

---
+++ odd64.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  int a,b,n;
+  cout<<"Enter your number";
+  cin>>a;
+  cout<<"enter your another number";
+  cin>>b;
+  n=a+b;
+  cout<<"the value is"<<n;
+  if(n%2==1)
+  {
+      cout<<"it is odd";
+  }
+  else
+  {
+      cout<<"it is even";
+  }
+  return 0;
+}
