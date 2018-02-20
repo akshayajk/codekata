3fdfdbbb167cc4a4b4b35d7105572a01fea875e9
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-20 21:51:21 +0530

    Create 38.cpp

---
+++ 38.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int i = 65;
+    int k = 120;
+    cout<<" value before swapping \t"<<i<<"\t"<<k<<"\n";
+
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    cout<<"value after swapping\t"<<i<<"\t"<<k;
+
+    return 0;
+}
