64506923c92348fa0ea3f3395ca00f5005bf3074
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-23 17:49:44 +0530

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num,i;
+char a[50];
+cout<<"Enter the string with number";
+cin>>a>>num;
+for(i=1;i<=num;i++)
+{
+cout<<a<<"\n";
+}
+
+return 0;
+}
+
