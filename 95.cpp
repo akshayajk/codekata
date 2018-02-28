9b2f6cd000aaa8ae057943564aba3a860e90667d
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-02-28 10:17:15 +0530

    Create 95.cpp

---
+++ 95.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        int p,t,r,a;
+        cout<<"enter the principal amount";
+        cin>>p;
+        cout<<"enter the time";
+        cin>>t;
+        cout<<"enter the rate of interest";
+        cin>>r;
+        
+      a=(p*r*t)/100;
+      cout<<a;
+    
+	// your code goes here
+	return 0;
+}
