8fdec21babb83473982c9b0bf49af4a97372d191
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-03-14 15:11:14 +0530

    Rename 112.cpp to 0112.cpp

---
+++ 0112.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,i,a[3],n;
+    cout<<"enter the two variables";
+    cin>>n>>k;
+    for(i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=1;i<=n;i++)
+    {
+    if(a[i]==k)
+    {
+        cout<<"yes";
+    }
+        
+    }
+}
