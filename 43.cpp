9be42d36817af1ba242b8a29436cf8f960f58c38
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-03-19 15:58:54 +0530

    Create 43.cpp

---
+++ 43.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str1,str2,str3;
+    cout<<"Enter the two strings";
+    cin>>str1>>str2;
+    str3 = str1+str2;
+    cout<<str3;
+    return 0;
+}
