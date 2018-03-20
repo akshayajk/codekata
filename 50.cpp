c7dee12b7a2c1a56b3487be868e27eaaffc117c0
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-03-20 10:10:27 +0530

    Create 50.cpp

---
+++ 50.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int i,result,set = 0;
+    cout<<"Enter a number :";
+    cin>>i;
+    for(int j = 0;j <= i;j++)
+    {
+        result = pow(2,j);
+        if(result == i)
+        {
+            set = 1;
+        }
+        else
+        {
+            set = 0;
+        }
+    }
+    if(set == 1)
+    {
+        cout<<yes;
+    }
+    else
+    {
+        cout<<no;
+    }
+    return 0;
+}
