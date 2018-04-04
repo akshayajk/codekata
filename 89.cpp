519651c01e893af3cc62f654af81b082e8c1c654
akshayajk <36322468+akshayajk@users.noreply.github.com>
2018-04-04 13:50:54 +0530

    Update 89.cpp

--- 89.cpp
+++ 89.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	char str[50],temp[50];
 	int i,k,j;
-cout<<"\nenter the string:";
-	cin>>str;
+cout<<"\nenter the string";
+cin>>str;
 	for(i=0;str[i]!='\0';i++)
 	{
 		for(j=i+1;str[j]!='\0';j++)
